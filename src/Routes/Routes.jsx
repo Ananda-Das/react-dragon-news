8ecc8eca1c9c0a02bd88824cb819b1399d4226c6
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-10-02 23:41:09 +0600

    React Project Folder Stucture and Router Setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
+import Home from "../pages/Home/Home";
+
+const Routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+        ]
+    }
+]);
+
+export default Routes;
