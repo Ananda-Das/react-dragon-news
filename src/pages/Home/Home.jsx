2002887e0fd57bc916a12ad9f8dc781681ff61aa
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-10-03 10:06:48 +0600

    Custome Tailwind class and font setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 // import React from 'react';
 
+import Header from "../shared/Header";
+import Navbar from "../shared/Navbar";
+
 const Home = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <Navbar></Navbar>
+            <h1 className="text-3xl font-poppins">This is home</h1>
         </div>
     );
 };
