98503c4c4a908f95139df7d278059ed3eb254370
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-10-03 10:29:39 +0600

    Homepage Layout and date time formatting using moment js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,27 @@
 // import React from 'react';
 
 import Header from "../shared/Header";
+import LeftSideNav from "../shared/LeftSideNav";
 import Navbar from "../shared/Navbar";
+import RightSideNav from "../shared/RightSideNav";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h1 className="text-3xl font-poppins">This is home</h1>
+            
+            <div className="grid lg:grid-cols-4">
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="col-span-2">
+                    <h1 className="text-3xl">News Comming Soon...</h1>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,9 +1,12 @@
-// import React from 'react';
+import logo from '../../assets/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is Header</h1>
+            <img src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
